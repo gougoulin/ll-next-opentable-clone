8a468c84193e4be06d76dc8ca9fbdb5ad715ab48
DESKTOP-9SK81HS\gougoulin <l>
2023-05-08 23:21:00 +1000

    fix: add comments

--- app/data/availableTime.ts
+++ app/data/availableTime.ts
@@ -1,13 +1,13 @@
 import dayjs, { Dayjs } from "dayjs";
 
 export default function availableTimes(open: Dayjs, close: Dayjs, now = dayjs()): string[] {
-  const start = open.set("minute", 0);
+  const start = open.set("minute", 0); // round up to the current hour
   let result: string[] = [];
-  let value = start;
+  let value = start; // value is always before or equal to open
   if (!start.isValid() || !value.isValid() || !close.isValid()) throw Error("Invalid date time");
   while (true) {
     if (value.isBefore(now)) continue; // skip past times
-    result.push(value.format("HH:mm:ssZ"));
+    result.push(value.format("HH:mm:ssZ")); // UTC time
     value = value.add(30, "minute");
     if (!value.isValid()) throw Error("Invalid date time");
     if (value.isAfter(close)) break; // stop at close time
